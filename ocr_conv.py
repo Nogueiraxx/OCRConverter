ebecd3da9c46642e566d34013d65e6aee3aa1e37
NogueiraZx <nogueira.neto1106@gmail.com>
2024-06-25 18:29:06 -0300

    Instalação de dependencias

--- ocr_conv.py
+++ ocr_conv.py
@@ -1,8 +1,16 @@
-import pytesseract
-import cv2
+import pytesseract 
+import cv2 as cv
+path = 'images/recorte_jornal2.png'
+imagem = cv.imread(path)
 
-imagem = cv2.imread("images\recorte_jornal3.png")
+pytesseract.pytesseract.tesseract_cmd = "C:/Program Files/Tesseract-OCR/tesseract.exe"
 
-texto = pytesseract.image_to_string(imagem)
-
-print(texto)
+if imagem is None:
+    print("Não foi possivel ler a imagem")
+else:
+    pdf = pytesseract.image_to_pdf_or_hocr(imagem, extension='pdf')
+    with open('imagem.pdf', 'w+b') as f:
+        f.write(pdf)
+    # texto = pytesseract.image_to_string(imagem)
+    # pytesseract.image_to_pdf_or_hocr(imagem)
+    # print(texto)
